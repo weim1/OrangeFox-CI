25cf82fba21f157f046b1f9a0863113c5fa616c3
weim1 <85015613+weim1@users.noreply.github.com>
2022-11-05 12:10:25 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_12.1_R"
+export DT_LINK="https://github.com/OrangeFoxRecovery/device_xiaomi_fleur.git -b fox_9.0"
 
-export DEVICE="beryllium"
+export DEVICE="fleur"
 export OEM="xiaomi"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
 ## "bootimage" - for A/B devices without recovery partition (and without vendor boot)
 ## "vendorbootimage" - for devices Using vendor boot for the recovery ramdisk (Usually for devices shipped with Android 12 or higher)
-export TARGET="recoveryimage"
+export TARGET="bootimage"
 
 export OUTPUT="OrangeFox*.zip"
 
@@ -22,7 +22,7 @@ DEPS=(
 )
 
 # Extra Command
-export EXTRA_CMD="export OF_MAINTAINER=Sushrut1101"
+export EXTRA_CMD="export OF_MAINTAINER=Weim"
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
